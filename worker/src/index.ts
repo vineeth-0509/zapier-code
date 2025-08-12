0b298e8cd95c17d57ec3a09c99b64d3abcf6108f
Vineeth-0509 <vineeththungani@gmail.com>
2025-08-12 23:15:00 +0530

    kafak producing events and making it consume and break and it continous after the break from the offset+1

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,15 +1,31 @@
-import {PrismaClient} from "@prisma/client";
-import {Kafka} from "kafkajs";
+import { PrismaClient } from "@prisma/client";
+import { Kafka } from "kafkajs";
 const client = new PrismaClient();
-const TOPIC_NAME ='zap_events'
+const TOPIC_NAME = "zap-events";
 const kafka = new Kafka({
-    clientId:"",
-    brokers:['localhost:9092']
-})
-async function main(){
-while(1){
-
+  clientId: "",
+  brokers: ["localhost:9092"],
+});
+async function main() {
+  const consumer = kafka.consumer({ groupId: "main-worker" });
+  await consumer.connect();
+  await consumer.subscribe({ topic: TOPIC_NAME, fromBeginning: true });
+  await consumer.run({
+    autoCommit: false,
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log({
+        partition,
+        offset: message.offset,
+        value: message.value?.toString(),
+      });
+      await new Promise((r) => setTimeout(r, 1000));
+      await consumer.commitOffsets([{
+        topic:TOPIC_NAME,
+        partition:partition,
+        offset: (parseInt(message.offset) + 1).toString()
+      }])
+    },
+  });
 }
-}
 
-main()
+main();
